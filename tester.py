0af1d5eb8d957c7806eda9ca2e54ba8d2b3efb7a
Dani Arribas-Bel <daniel.arribas.bel@gmail.com>
2016-08-14 00:09:40 +0100

    Adding more testing scripts

--- tester.py
+++ tester.py
@@ -1,3 +1,170 @@
-import numpy; print(numpy)
-import geopandas as gpd; print(gpd)
-import pandas as pd; print(pd)
+
+# coding: utf-8
+
+# # Check `GDS` stack
+# 
+# This notebook checks all software requirements for the course Geographic Data Science are correctly installed. 
+# 
+# A successful run of the notebook implies no errors returned in any cell *and* every cell beyond the first one returning a printout of `True`. This ensures a correct environment installed.
+
+# In[2]:
+
+get_ipython().system(u' which python')
+get_ipython().system(u' which jupyter')
+
+
+# In[3]:
+
+import numpy; print(numpy.__version__); print(numpy)
+
+
+# In[4]:
+
+import geopandas; print(geopandas.__version__); print(geopandas)
+
+
+# In[5]:
+
+import pandas; print(pandas.__version__); print(pandas)
+
+
+# ---
+
+# In[1]:
+
+import bokeh as bk
+float(bk.__version__[:4]) >= 0.12
+
+
+# In[2]:
+
+import matplotlib as mpl
+float(mpl.__version__[:3]) >= 1.5
+
+
+# In[3]:
+
+import mplleaflet as mpll
+
+
+# In[4]:
+
+import seaborn as sns
+float(sns.__version__[:3]) >= 0.6
+
+
+# ---
+
+# In[5]:
+
+import qgrid
+
+
+# In[6]:
+
+import pandas as pd
+float(pd.__version__[:4]) >= 0.18
+
+
+# In[7]:
+
+import sklearn
+float(sklearn.__version__[:4]) >= 0.17
+
+
+# In[8]:
+
+import statsmodels.api as sm
+float(sm.version.version[:3]) >= 0.6
+
+
+# In[9]:
+
+import xlrd
+
+
+# In[10]:
+
+import xlsxwriter
+
+
+# ---
+
+# In[11]:
+
+import clusterpy as cl
+float(cl.__version__[:3]) >= 1.0
+
+
+# In[12]:
+
+import fiona
+float(fiona.__version__[:3]) >= 1.7
+
+
+# In[13]:
+
+import geopandas as gpd
+float(gpd.__version__[:3]) >= 0.2
+
+
+# In[14]:
+
+import pysal as ps
+float(ps.version[:4]) >= 1.11
+
+
+# In[20]:
+
+import rasterio as rio
+
+
+# # Test
+
+# In[15]:
+
+shp = ps.examples.get_path('columbus.shp')
+db = gpd.read_file(shp)
+db.head()
+
+
+# In[16]:
+
+import matplotlib.pyplot as plt
+get_ipython().magic(u'matplotlib inline')
+f, ax = plt.subplots(1)
+for poly in db['geometry']:
+    gpd.plotting.plot_multipolygon(ax, poly, facecolor='yellow', edgecolor='grey', linewidth=0.1)
+ax.set_axis_off()
+plt.show()
+
+
+# In[17]:
+
+db.crs['init'] = 'epsg:26918'
+
+
+# In[18]:
+
+db_wgs84 = db.to_crs(epsg=4326)
+db_wgs84.plot()
+plt.show()
+
+
+# In[19]:
+
+db.plot(column='INC', scheme='fisher_jenks', cmap=plt.matplotlib.cm.Blues)
+plt.show()
+
+
+# import numpy as np
+# source = rio.open('../labs/figs/lab03_GBOverview.tif', 'r')
+# red = source.read(1)
+# green = source.read(2)
+# blue = source.read(3)
+# pix = np.dstack((red, green, blue))
+# bounds = (source.bounds.left, source.bounds.right, \
+#           source.bounds.bottom, source.bounds.top)
+# f = plt.figure(figsize=(6, 6))
+# ax = plt.imshow(pix, extent=bounds)
+
