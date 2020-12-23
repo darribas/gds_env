db6f31beb31ed8dc4db986b7fe7a28184d4d218a
Dani Arribas-Bel <daniel.arribas.bel@gmail.com>
2020-12-23 12:25:10 +0000

    Move R build to .sh scripts and split between stack and GDS layers

---
+++ gds/install_R_gds.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+set -e
+
+# always set this for scripts but don't declare as ENV..
+export DEBIAN_FRONTEND=noninteractive
+
+#--- Utilities ---#
+
+apt-get update -qq \
+  && apt-get install -y --no-install-recommends software-properties-common \
+  && add-apt-repository -y ppa:ubuntugis/ubuntugis-experimental \
+  && apt-get update -qq \
+  && apt-get install -y --no-install-recommends \
+    gpg-agent \
+    jq \
+    libatk1.0-0 \
+    libv8-dev \
+    libx11-6 \
+    libxtst6 \
+ && apt-get autoclean \
+ && apt-get autoremove \
+ && apt-get clean \
+ && rm -rf /var/lib/apt/lists/*
+
+#--- R ---#
+# https://github.com/rocker-org/rocker-versioned/blob/master/r-ver/Dockerfile
+# Look at: http://sites.psu.edu/theubunturblog/installing-r-in-ubuntu/
+
+echo "deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/" >> /etc/apt/sources.list \
+ && sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9 \
+ && apt-get update \
+ && apt-get install -y \
+    r-base \
+    r-base-dev \
+ && apt-get autoclean \
+ && apt-get autoremove \
+ && apt-get clean \
+ && rm -rf /var/lib/apt/lists/*
+
+#--- R libs ---#
+# SAN requirements (on top of Rocker Geospatial)
+# https://github.com/GDSL-UL/san/blob/master/01-overview.Rmd
+R -e "install.packages(c( \
+            'arm', \
+            'car', \
+            'corrplot', \
+            'FRK', \
+            'gghighlight', \
+            'ggmap', \
+            'GISTools', \
+            'jtools', \
+            'kableExtra', \
+            'knitr', \
+            'lme4', \
+            'lmtest', \
+            'lubridate', \
+            'MASS', \
+            'merTools', \
+            'sjPlot', \
+            'spgwr', \
+            'stargazer', \
+            'viridis' \
+            ), repos='https://cran.rstudio.com');"
+# ------------------
+# Other
+R -e "install.packages(c( \
+            'areal', \
+            'deldir', \
+            'feather', \
+            'geojsonio', \
+            'hexbin', \
+            'igraph', \
+            'nlme', \
+            'randomForest', \
+            'RCurl', \
+            'rpostgis', \
+            'shiny', \
+            'splancs', \
+            'TraMineR', \
+            'tufte' \
+            ), repos='https://cran.rstudio.com');"
+
+## Geocomputation in R meta-package
+R -e "library(devtools); \
+      devtools::install_github('geocompr/geocompkg');"
+
---
+++ gds/install_R_stack.sh
@@ -0,0 +1,148 @@
+#!/bin/bash
+
+set -e
+
+# always set this for scripts but don't declare as ENV..
+export DEBIAN_FRONTEND=noninteractive
+
+#--- Utilities ---#
+
+# General
+# https://github.com/rocker-org/rocker-versioned2/blob/7e065a9d71b8ff2561f7f53c107129246864021e/scripts/install_R_ppa.sh
+apt-get update -qq \
+  && apt-get install -y --no-install-recommends software-properties-common \
+  && apt-get update -qq \
+  && apt-get install -y --no-install-recommends \
+    ca-certificates \
+    less \
+    libopenblas-base \
+    locales \
+    wget \
+    dirmngr \
+    gpg \
+    gpg-agent
+
+#--- R ---#
+# https://github.com/rocker-org/rocker-versioned/blob/master/r-ver/Dockerfile
+# Look at: http://sites.psu.edu/theubunturblog/installing-r-in-ubuntu/
+
+echo "deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/" >> /etc/apt/sources.list \
+ && sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9 \
+ && apt-get update \
+ && apt-get install -y \
+    r-base \
+    r-base-dev \
+    libcurl4-openssl-dev \
+ && apt-get autoclean \
+ && apt-get autoremove \
+ && apt-get clean \
+ && rm -rf /var/lib/apt/lists/*
+
+#--- R libs ---#
+
+        ### TidyVerse ###
+# https://github.com/rocker-org/rocker-versioned2/blob/65309d4d9dac11d3a78dd26ee4bf7b715436db31/scripts/install_tidyverse.sh
+apt-get update -qq \
+  && apt-get install -y --no-install-recommends \
+    libxml2-dev \
+    libcairo2-dev \
+    libgit2-dev \
+    default-libmysqlclient-dev \
+    libpq-dev \
+    libsasl2-dev \
+    libsqlite3-dev \
+    libssh2-1-dev \
+    unixodbc-dev
+
+R -e "install.packages(c( \
+            'tidyverse', \
+            'devtools', \
+            'rmarkdown', \
+            'BiocManager', \
+            'vroom', \
+            'gert' \
+            ), repos='https://cran.rstudio.com');"
+
+# Dplyr database backends (skip nycflights13, Lahman as it's data)
+R -e "install.packages(c( \
+            'arrow', \
+            'dbplyr', \
+            'DBI', \
+            'dtplyr', \
+            'RMariaDB', \
+            'RPostgres', \
+            'RSQLite', \
+            'fst' \
+            ), repos='https://cran.rstudio.com');"
+
+        ### Geospatial ###
+# https://github.com/rocker-org/rocker-versioned2/blob/dee65b3f0cb1cae3a185d7b54342abef276a4643/scripts/install_geospatial.sh
+# Skip wgrib2 as not relevant
+apt-get update -qq \
+  && apt-get install -y --no-install-recommends \
+    gdal-bin \
+    lbzip2 \
+    libfftw3-dev \
+    libgdal-dev \
+    libgeos-dev \
+    libgsl0-dev \
+    libgl1-mesa-dev \
+    libglu1-mesa-dev \
+    libhdf4-alt-dev \
+    libhdf5-dev \
+    libjq-dev \
+    libpq-dev \
+    libproj-dev \
+    libprotobuf-dev \
+    libnetcdf-dev \
+    libsqlite3-dev \
+    libssl-dev \
+    libudunits2-dev \
+    lsb-release \
+    netcdf-bin \
+    postgis \
+    protobuf-compiler \
+    sqlite3 \
+    tk-dev \
+    unixodbc-dev
+
+R -e "install.packages(c( \
+            'RColorBrewer', \
+            'Randomfields', \
+            'RNetCDF', \
+            'classInt', \
+            'deldir', \
+            'gstat', \
+            'hdf5r', \
+            'lidR', \
+            'mapdata', \
+            'maptools', \
+            'mapview', \
+            'ncdf4', \
+            'proj4', \
+            'raster', \
+            'rgdal', \
+            'rgeos', \
+            'rlas', \
+            'sf', \
+            'sp', \
+            'spacetime', \
+            'spatstat', \
+            'spatialreg', \
+            'spdep', \
+            'stars', \
+            'tidync', \
+            'tmap', \
+            'geoR', \
+            'geosphere' \
+            ), repos='https://cran.rstudio.com');"
+
+## from bioconductor
+R -e "library(BiocManager); \
+      BiocManager::install('rhdf5');"
+
+# Clean up
+apt-get autoclean \
+ && apt-get autoremove \
+ && apt-get clean \
+ && rm -rf /var/lib/apt/lists/*
