490a1568801283ed1bd29d9fda54a7c96e48c41e
Dani Arribas-Bel <daniel.arribas.bel@gmail.com>
2021-04-07 10:52:11 +0100

    conda-env --> conda env

--- gds_py/gds_py_win_installer.bat
+++ gds_py/gds_py_win_installer.bat
@@ -1,7 +1,7 @@
 REM Please run this from the Anaconda Prompt
 REM This installer is pinned to the latest released version
 REM Set up GDS environment
-conda-env create -f https://github.com/darribas/gds_env/raw/v6.1/gds_py/gds_py.yml
+conda env create -f https://github.com/darribas/gds_env/raw/v6.1/gds_py/gds_py.yml
 REM Add pip packages
 conda install -y -c conda-forge git
 pip install -r https://github.com/darribas/gds_env/raw/v6.1/gds_py/gds_py_pip.txt
