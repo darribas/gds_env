298725becda1f90b10258ef45d3dcd4fdb095779
Dani Arribas-Bel <daniel.arribas.bel@gmail.com>
2022-04-13 10:41:49 +0100

    Point Win installer to master

--- gds_py/gds_py_win_installer.bat
+++ gds_py/gds_py_win_installer.bat
@@ -1,12 +1,12 @@
 REM Please run this from the Anaconda Prompt
 REM This installer is pinned to the latest released version
 REM Set up GDS environment
-call conda env create -f https://github.com/darribas/gds_env/raw/v7.0/gds_py/gds_py.yml
+call conda env create -f https://raw.githubusercontent.com/darribas/gds_env/master/gds_py/gds_py.yml
 REM Activate environment
 call conda activate gds
 REM Add pip packages
 call conda install -y -c conda-forge git
-pip install --user -r https://github.com/darribas/gds_env/raw/v7.0/gds_py/gds_py_pip.txt
+pip install --user -r https://raw.githubusercontent.com/darribas/gds_env/master/gds_py/gds_py_pip.txt
 REM JupyterLab old plugin's
 jupyter labextension install nbdime-jupyterlab --no-build
 jupyter lab build -y
