e4f8742995ac3a6a2108d97f10c5ea606d7a4878
Dani Arribas-Bel <daniel.arribas.bel@gmail.com>
2023-10-19 08:52:47 +0000

    Update of testing w/ new python installer stack

--- gds_py/gds_py_win_installer.bat
+++ gds_py/gds_py_win_installer.bat
@@ -1,15 +1,8 @@
 REM Please run this from the Anaconda Prompt
-REM This installer is pinned to the latest released version
+REM This installer uses the latest version in master
 REM Set up GDS environment
 call conda env create -f https://raw.githubusercontent.com/darribas/gds_env/master/gds_py/gds_py.yml
-REM Activate environment
-call conda activate gds
-REM Add pip packages
-call conda install -y -c conda-forge git
-pip install --user -r https://raw.githubusercontent.com/darribas/gds_env/master/gds_py/gds_py_pip.txt
 REM JupyterLab old plugin's
-jupyter labextension install nbdime-jupyterlab --no-build
-jupyter lab build -y
-jupyter lab clean -y
+jupyter labextension disable "@jupyterlab/apputils-extension:announcements"
 conda clean --all -f -y
 call conda deactivate
