6058d0e4f3a997a2ac8207e9b2b1839373eac175
Dani Arribas-Bel <daniel.arribas.bel@gmail.com>
2023-10-19 12:16:07 +0100

    activate gds in windows

--- gds_py/gds_py_win_installer.bat
+++ gds_py/gds_py_win_installer.bat
@@ -3,6 +3,7 @@ REM This installer uses the latest version in master
 REM Set up GDS environment
 call conda env create -f https://raw.githubusercontent.com/darribas/gds_env/master/gds_py/gds_py.yml
 REM JupyterLab old plugin's
+call conda activate gds
 jupyter labextension disable "@jupyterlab/apputils-extension:announcements"
 conda clean --all -f -y
 call conda deactivate
