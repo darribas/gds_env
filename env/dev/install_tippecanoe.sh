8af68656fade5cbc213f509d7c38d7e944d195b3
Dani Arribas-Bel <daniel.arribas.bel@gmail.com>
2024-07-04 13:57:46 +0000

    Add new conda-based build

--- env/dev/install_tippecanoe.sh
+++ env/dev/install_tippecanoe.sh
@@ -1,3 +1,6 @@
+apt-get update \
+ && apt-get -y install build-essential libsqlite3-dev zlib1g-dev
+
 git clone https://github.com/felt/tippecanoe.git $HOME/tippecanoe \
  && cd $HOME/tippecanoe \
  && make -j
